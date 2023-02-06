7fa91eaa9789b085047f1a99e211db1929bb2434
Nikolay Matrosov <nikthespirit@yandex-team.ru>
2023-02-06 20:39:17 +0100

    fixed middleware

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,9 +1,9 @@
 import {
-  CompleteMultipartUploadCommand,
-  CreateMultipartUploadCommand,
-  PutObjectCommand,
-  S3Client,
-  UploadPartCommand,
+    CompleteMultipartUploadCommand,
+    CreateMultipartUploadCommand,
+    PutObjectCommand,
+    S3Client,
+    UploadPartCommand,
 } from '@aws-sdk/client-s3';
 import {expect, test} from '@jest/globals';
 import * as cp from 'child_process';
@@ -15,187 +15,203 @@ import {upload, UploadInputs} from '../src/main';
 // This test will run only in fully configured env and creates real VM
 // in the Yandex Cloud, so it will be disabled in CI/CD. You can enable it to test locally.
 test.skip('test runs', () => {
-  process.env['INPUT_INCLUDE'] = '.\n./package.json';
-  process.env['INPUT_EXCLUDE'] = '**/*.txt\n**/*.yaml\n**/*.ts';
-  process.env['INPUT_TAGS'] = 'foo\nbar';
+    process.env['INPUT_INCLUDE'] = '.\n./package.json';
+    process.env['INPUT_EXCLUDE'] = '**/*.txt\n**/*.yaml\n**/*.ts';
+    process.env['INPUT_TAGS'] = 'foo\nbar';
 
-  const np = process.execPath;
-  const ip = path.join(__dirname, '..', 'lib', 'main.js');
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env,
-    cwd: __dirname,
-  };
-  let res;
-  try {
-    res = cp.execFileSync(np, [ip], options);
-  } catch (e) {
-    console.log((e as any).stdout.toString());
-    console.log((e as any).stderr.toString());
-  }
-  console.log(res?.toString());
+    const np = process.execPath;
+    const ip = path.join(__dirname, '..', 'lib', 'main.js');
+    const options: cp.ExecFileSyncOptions = {
+        env: process.env,
+        cwd: __dirname,
+    };
+    let res;
+    try {
+        res = cp.execFileSync(np, [ip], options);
+    } catch (e) {
+        console.log((e as any).stdout.toString());
+        console.log((e as any).stderr.toString());
+    }
+    console.log(res?.toString());
 });
 
 describe('upload', function () {
-  const s3client = new S3Client({});
-  const mockedSendFn = jest.spyOn(s3client, 'send');
+    const s3client = new S3Client({});
+    const mockedSendFn = jest.spyOn(s3client, 'send');
 
-  beforeEach(() => {
-    mockedSendFn.mockReset();
-  });
-
-  test('it should add files from include', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: '',
-      include: ['./src/*'],
-      exclude: [],
-      root: '.',
-    };
-
-    await upload(s3client, inputs);
-
-    expect(mockedSendFn).toBeCalledTimes(3);
-    const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
-    expect(keys).toEqual(['src/exclude.txt', 'src/exclude.yaml', 'src/func.js']);
-  });
-
-  test('it should drop files from if they do not match include patterns', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: '',
-      include: ['./src/*.js'],
-      exclude: [],
-      root: '.',
-    };
-
-    await upload(s3client, inputs);
-
-    expect(mockedSendFn).toBeCalledTimes(1);
-    const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
-    expect(keys).toEqual(['src/func.js']);
-  });
-
-  test('it should drop files from if they match exclude patterns', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: '',
-      include: ['./src/*'],
-      exclude: ['**/*.txt'],
-      root: '.',
-    };
-
-    await upload(s3client, inputs);
-
-    expect(mockedSendFn).toBeCalledTimes(2);
-    const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
-    expect(keys).toEqual(['src/exclude.yaml', 'src/func.js']);
-  });
-
-  test('it should drop folder prefix if sourceRoot provided', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: '',
-      include: ['*'],
-      exclude: [],
-      root: './src',
-    };
-
-    await upload(s3client, inputs);
-
-    expect(mockedSendFn).toBeCalledTimes(3);
-    const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
-    expect(keys).toEqual(['exclude.txt', 'exclude.yaml', 'func.js']);
-  });
-
-  test('it should respect source root and include only needed files', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: '',
-      include: ['*.js'],
-      exclude: [],
-      root: './src',
-    };
-
-    await upload(s3client, inputs);
-
-    expect(mockedSendFn).toBeCalledTimes(1);
-    const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
-    expect(keys).toEqual(['func.js']);
-  });
-
-  test('it should add prefix', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: 'prefix/',
-      include: ['*.js'],
-      exclude: [],
-      root: './src',
-    };
-
-    await upload(s3client, inputs);
-
-    expect(mockedSendFn).toBeCalledTimes(1);
-    const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
-    expect(keys).toEqual(['prefix/func.js']);
-  });
-
-  jest.setTimeout(600_000);
-  test('it should use multipart on big files', async () => {
-    const inputs: UploadInputs = {
-      bucket: 'bucket',
-      prefix: '',
-      include: ['10mbfile.txt'],
-      exclude: [],
-      root: './bigfile',
-    };
-    const cwd = process.env.GITHUB_WORKSPACE ?? '';
-    let createCommands = 0;
-    let uploadCommands = 0;
-    let completeCommands = 0;
-
-    let createMultipartUploadCommand: CreateMultipartUploadCommand | undefined;
-
-    mockedSendFn.mockImplementation(cmd => {
-      if (cmd instanceof CreateMultipartUploadCommand) {
-        createCommands += 1;
-        createMultipartUploadCommand = cmd;
-        return Promise.resolve({
-          UploadId: 1,
-        });
-      }
-      if (cmd instanceof UploadPartCommand) {
-        uploadCommands += 1;
-        return Promise.resolve({
-          ETag: Math.random().toString().slice(2),
-        });
-      }
-      if (cmd instanceof CompleteMultipartUploadCommand) {
-        completeCommands += 1;
-        return Promise.resolve({
-          ETag: Math.random().toString().slice(2),
-        });
-      }
+    beforeEach(() => {
+        mockedSendFn.mockReset();
     });
 
-    try {
-      fs.mkdirSync(path.join(cwd, './bigfile'));
-    } catch (e: any) {
-      if (e.code !== 'EEXIST') {
-        console.log(e);
-        throw e;
-      }
-    }
-    fs.writeFileSync(path.join(cwd, './bigfile/10mbfile.txt'), new Uint8Array(10 * 1024 ** 2));
+    test('it should add files from include', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['./src/*'],
+            exclude: [],
+            root: '.',
+        };
 
-    await upload(s3client, inputs);
+        await upload(s3client, inputs);
 
-    expect(mockedSendFn).toBeCalledTimes(5);
-    expect(createCommands).toEqual(1);
-    expect(uploadCommands).toEqual(3);
-    expect(completeCommands).toEqual(1);
-    if (!createMultipartUploadCommand) {
-      throw new Error('createMultipartUploadCommand === null');
-    }
-    expect(createMultipartUploadCommand.input.Key).toEqual('10mbfile.txt');
-  });
+        expect(mockedSendFn).toBeCalledTimes(3);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['src/exclude.txt', 'src/exclude.yaml', 'src/func.js']);
+    });
+
+    test('it should drop files from if they do not match include patterns', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['./src/*.js'],
+            exclude: [],
+            root: '.',
+        };
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(1);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['src/func.js']);
+    });
+
+    test('it should drop files from if they match exclude patterns', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['./src/*'],
+            exclude: ['**/*.txt'],
+            root: '.',
+        };
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(2);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['src/exclude.yaml', 'src/func.js']);
+    });
+
+    test('it should drop folder prefix if sourceRoot provided', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['*'],
+            exclude: [],
+            root: './src',
+        };
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(3);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['exclude.txt', 'exclude.yaml', 'func.js']);
+    });
+
+    test('it handle folders', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['src'],
+            exclude: [],
+            root: '.',
+        };
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(3);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['src/exclude.txt', 'src/exclude.yaml', 'src/func.js']);
+    });
+
+    test('it should respect source root and include only needed files', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['*.js'],
+            exclude: [],
+            root: './src',
+        };
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(1);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['func.js']);
+    });
+
+    test('it should add prefix', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: 'prefix/',
+            include: ['*.js'],
+            exclude: [],
+            root: './src',
+        };
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(1);
+        const keys = mockedSendFn.mock.calls.map(([cmd]) => (cmd as PutObjectCommand).input.Key).sort();
+        expect(keys).toEqual(['prefix/func.js']);
+    });
+
+    jest.setTimeout(600_000);
+    test('it should use multipart on big files', async () => {
+        const inputs: UploadInputs = {
+            bucket: 'bucket',
+            prefix: '',
+            include: ['10mbfile.txt'],
+            exclude: [],
+            root: './bigfile',
+        };
+        const cwd = process.env.GITHUB_WORKSPACE ?? '';
+        let createCommands = 0;
+        let uploadCommands = 0;
+        let completeCommands = 0;
+
+        let createMultipartUploadCommand: CreateMultipartUploadCommand | undefined;
+
+        mockedSendFn.mockImplementation(cmd => {
+            if (cmd instanceof CreateMultipartUploadCommand) {
+                createCommands += 1;
+                createMultipartUploadCommand = cmd;
+                return Promise.resolve({
+                    UploadId: 1,
+                });
+            }
+            if (cmd instanceof UploadPartCommand) {
+                uploadCommands += 1;
+                return Promise.resolve({
+                    ETag: Math.random().toString().slice(2),
+                });
+            }
+            if (cmd instanceof CompleteMultipartUploadCommand) {
+                completeCommands += 1;
+                return Promise.resolve({
+                    ETag: Math.random().toString().slice(2),
+                });
+            }
+        });
+
+        try {
+            fs.mkdirSync(path.join(cwd, './bigfile'));
+        } catch (e: any) {
+            if (e.code !== 'EEXIST') {
+                console.log(e);
+                throw e;
+            }
+        }
+        fs.writeFileSync(path.join(cwd, './bigfile/10mbfile.txt'), new Uint8Array(10 * 1024 ** 2));
+
+        await upload(s3client, inputs);
+
+        expect(mockedSendFn).toBeCalledTimes(5);
+        expect(createCommands).toEqual(1);
+        expect(uploadCommands).toEqual(3);
+        expect(completeCommands).toEqual(1);
+        if (!createMultipartUploadCommand) {
+            throw new Error('createMultipartUploadCommand === null');
+        }
+        expect(createMultipartUploadCommand.input.Key).toEqual('10mbfile.txt');
+    });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core';
 import {
-  type AbortMultipartUploadCommandOutput,
-  type CompleteMultipartUploadCommandOutput,
-  S3Client,
+    type AbortMultipartUploadCommandOutput,
+    type CompleteMultipartUploadCommandOutput,
+    S3Client,
 } from '@aws-sdk/client-s3';
 import {Upload} from '@aws-sdk/lib-storage';
+import {HttpRequest} from '@aws-sdk/protocol-http';
+import {type FinalizeRequestMiddleware} from '@aws-sdk/types/dist-types/middleware';
 
 import {IamTokenService} from '@yandex-cloud/nodejs-sdk/dist/token-service/iam-token-service';
 import * as fs from 'fs';
@@ -15,132 +17,155 @@ import path from 'node:path';
 import {fromServiceAccountJsonFile} from './service-account-json';
 
 type ActionInputs = {
-  bucket: string;
-  prefix: string;
-  root: string;
-  include: string[];
-  exclude: string[];
+    bucket: string;
+    prefix: string;
+    root: string;
+    include: string[];
+    exclude: string[];
 };
 
 async function run(): Promise<void> {
-  try {
-    const ycSaJsonCredentials = core.getInput('yc-sa-json-credentials', {
-      required: true,
-    });
-    core.setSecret(ycSaJsonCredentials);
+    try {
+        const ycSaJsonCredentials = core.getInput('yc-sa-json-credentials', {
+            required: true,
+        });
+        core.setSecret(ycSaJsonCredentials);
 
-    const serviceAccountJson = fromServiceAccountJsonFile(JSON.parse(ycSaJsonCredentials));
+        const serviceAccountJson = fromServiceAccountJsonFile(JSON.parse(ycSaJsonCredentials));
 
-    const inputs: ActionInputs = {
-      bucket: core.getInput('bucket', {required: true}),
-      prefix: core.getInput('prefix', {required: false}),
-      root: core.getInput('root', {required: true}),
-      include: core.getMultilineInput('include', {required: false}),
-      exclude: core.getMultilineInput('exclude', {required: false}),
-    };
+        const inputs: ActionInputs = {
+            bucket: core.getInput('bucket', {required: true}),
+            prefix: core.getInput('prefix', {required: false}),
+            root: core.getInput('root', {required: true}),
+            include: core.getMultilineInput('include', {required: false}),
+            exclude: core.getMultilineInput('exclude', {required: false}),
+        };
 
-    // Initialize Token service with your SA credentials
-    const tokenService = new IamTokenService(serviceAccountJson);
+        // Initialize Token service with your SA credentials
+        const tokenService = new IamTokenService(serviceAccountJson);
 
-    const s3Client = new S3Client({
-      region: 'ru-central1',
-      endpoint: 'https://storage.yandexcloud.net',
-    });
+        const s3Client = new S3Client({
+            region: 'ru-central1',
+            endpoint: 'https://storage.yandexcloud.net',
+        });
 
-    s3Client.middlewareStack.removeByTag('AWSAUTH');
-    s3Client.middlewareStack.add(next => async args => {
-      // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-      (args as any).request.headers['X-YaCloud-SubjectToken'] = tokenService.getToken();
-      return next(args);
-    });
+        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+        const middleware: FinalizeRequestMiddleware<any, any> = next => {
+            return async args => {
+                if (!HttpRequest.isInstance(args.request)) {
+                    return next(args);
+                }
+                args.request.headers['X-YaCloud-SubjectToken'] = await tokenService.getToken();
+                return next(args);
+            };
+        };
 
-    await upload(s3Client, inputs);
-  } catch (error) {
-    if (error instanceof Error) {
-      core.setFailed(error.message);
+        s3Client.middlewareStack.removeByTag('AWSAUTH');
+        s3Client.middlewareStack.addRelativeTo(middleware, {
+            name: 'ycAuthMiddleware',
+            tags: ['YCAUTH'],
+            relation: 'after',
+            toMiddleware: 'retryMiddleware',
+            override: true,
+        });
+
+        await upload(s3Client, inputs);
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
     }
-  }
 }
 
 export interface UploadInputs {
-  include: string[];
-  exclude: string[];
-  root: string;
-  prefix: string;
-  bucket: string;
+    include: string[];
+    exclude: string[];
+    root: string;
+    prefix: string;
+    bucket: string;
 }
 
 const uploadFile = async (
-  client: S3Client,
-  filePath: string,
-  {root, bucket, prefix}: UploadInputs,
+    client: S3Client,
+    filePath: string,
+    {root, bucket, prefix}: UploadInputs,
 ): Promise<CompleteMultipartUploadCommandOutput | AbortMultipartUploadCommandOutput | undefined> => {
-  const contentType = mimeTypes.lookup(filePath) || 'text/plain';
+    const contentType = mimeTypes.lookup(filePath) || 'text/plain';
 
-  let key = path.relative(root, filePath);
-  if (prefix) {
-    key = path.join(prefix, key);
-  }
-  try {
-    core.info(`starting to upload ${key}`);
-    const parallelUploads3 = new Upload({
-      client,
-      params: {
-        Bucket: bucket,
-        Key: key,
-        Body: fs.createReadStream(filePath),
-        ContentType: contentType,
-      },
-      queueSize: 4,
-      leavePartsOnError: false,
-    });
+    let key = path.relative(root, filePath);
+    if (prefix) {
+        key = path.join(prefix, key);
+    }
+    try {
+        core.info(`starting to upload ${key}`);
+        const parallelUploads3 = new Upload({
+            client,
+            params: {
+                Bucket: bucket,
+                Key: key,
+                Body: fs.createReadStream(filePath),
+                ContentType: contentType,
+            },
+            queueSize: 4,
+            leavePartsOnError: false,
+        });
 
-    return await parallelUploads3.done();
-  } catch (e) {
-    core.error(`${e}`);
-  }
+        return await parallelUploads3.done();
+    } catch (e) {
+        core.error(`${e}`);
+    }
 };
 
 export async function upload(s3Client: S3Client, inputs: UploadInputs): Promise<void> {
-  core.startGroup('Upload');
+    core.startGroup('Upload');
 
-  try {
-    core.info('Upload start');
+    try {
+        core.info('Upload start');
 
-    const workspace = process.env['GITHUB_WORKSPACE'] ?? '';
-    const patterns = parseIgnoreGlobPatterns(inputs.exclude);
-    const root = path.join(workspace, inputs.root);
+        const workspace = process.env['GITHUB_WORKSPACE'] ?? '';
+        const patterns = parseIgnoreGlobPatterns(inputs.exclude);
+        const root = path.join(workspace, inputs.root);
 
-    for (const include of inputs.include) {
-      const pathFromSourceRoot = path.join(root, include);
-      const matches = glob.sync(pathFromSourceRoot, {absolute: false});
-      for (const match of matches) {
-        const res = !patterns.map(p => minimatch(match, p, {matchBase: true})).some(x => x);
-        if (res) {
-          await uploadFile(s3Client, match, {
-            ...inputs,
-            root,
-          });
+        for (const include of inputs.include) {
+            let pathFromSourceRoot = path.join(root, include);
+            if (!pathFromSourceRoot.includes('*')) {
+                try {
+                    const stat = fs.statSync(pathFromSourceRoot);
+                    if (stat.isDirectory()) {
+                        pathFromSourceRoot = path.join(pathFromSourceRoot, '*');
+                    }
+                } catch (e) {
+                    core.debug(`${e}`);
+                }
+            }
+            const matches = glob.sync(pathFromSourceRoot, {absolute: false});
+            for (const match of matches) {
+                const res = !patterns.map(p => minimatch(match, p, {matchBase: true})).some(x => x);
+                if (res) {
+                    await uploadFile(s3Client, match, {
+                        ...inputs,
+                        root,
+                    });
+                }
+            }
         }
-      }
+    } finally {
+        core.endGroup();
     }
-  } finally {
-    core.endGroup();
-  }
 }
 
 function parseIgnoreGlobPatterns(patterns: string[]): string[] {
-  const result: string[] = [];
+    const result: string[] = [];
 
-  for (const pattern of patterns) {
-    //only not empty patterns
-    if (pattern?.length > 0) {
-      result.push(pattern);
+    for (const pattern of patterns) {
+        //only not empty patterns
+        if (pattern?.length > 0) {
+            result.push(pattern);
+        }
     }
-  }
 
-  core.info(`Source ignore pattern: "${JSON.stringify(result)}"`);
-  return result;
+    core.info(`Source ignore pattern: "${JSON.stringify(result)}"`);
+    return result;
 }
 
 run();
