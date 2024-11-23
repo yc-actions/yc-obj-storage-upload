856051e31da50d766fc084a0e65a11dd34d9ebac
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2024-11-23 17:26:29 +0100

    feat: add cache-control config (#457)
    * feat: add cache-control config
    closes #456
    
    * feat: fix matching behavior
    
    * chore: add dist

---
+++ __tests__/cache-contol.test.ts
@@ -0,0 +1,116 @@
+import { getCacheControlValue, parseCacheControlFormats } from '../src/cache-control'
+
+describe('Cache-Control', () => {
+    describe('parseCacheControlFormats', () => {
+        it('should parse cache control formats', () => {
+            const formats = ['*.html:public, max-age=3600', '*.css:public, max-age=3600', '*:public, max-age=3600']
+            const result = parseCacheControlFormats(formats)
+
+            expect(result).toEqual({
+                mapping: new Map([
+                    ['*.html', 'public, max-age=3600'],
+                    ['*.css', 'public, max-age=3600']
+                ]),
+                default: 'public, max-age=3600'
+            })
+        })
+
+        it('should handle empty value', () => {
+            const formats = ['*.html:public, max-age=3600', '*.css:public, max-age=3600', '*:']
+            const result = parseCacheControlFormats(formats)
+
+            expect(result).toEqual({
+                mapping: new Map([
+                    ['*.html', 'public, max-age=3600'],
+                    ['*.css', 'public, max-age=3600']
+                ]),
+                default: undefined
+            })
+        })
+
+        it('should handle empty string', () => {
+            const formats = ['*.html:public, max-age=3600', '*.css:public, max-age=3600', '*: ']
+            const result = parseCacheControlFormats(formats)
+
+            expect(result).toEqual({
+                mapping: new Map([
+                    ['*.html', 'public, max-age=3600'],
+                    ['*.css', 'public, max-age=3600']
+                ]),
+                default: undefined
+            })
+        })
+
+        it('should handle empty input', () => {
+            const formats: string[] = []
+            const result = parseCacheControlFormats(formats)
+
+            expect(result).toEqual({
+                mapping: new Map(),
+                default: undefined
+            })
+        })
+
+        it('should handle multiple keys separated by comma', () => {
+            const formats = [
+                '*.html, *.htm:public, max-age=3600',
+                '*.css:public, max-age=3600',
+                '*:public, max-age=3600'
+            ]
+            const result = parseCacheControlFormats(formats)
+
+            expect(result).toEqual({
+                mapping: new Map([
+                    ['*.html', 'public, max-age=3600'],
+                    ['*.htm', 'public, max-age=3600'],
+                    ['*.css', 'public, max-age=3600']
+                ]),
+                default: 'public, max-age=3600'
+            })
+        })
+    })
+
+    describe('getCacheControlValue', () => {
+        it('should return value for key', () => {
+            const cacheControl = {
+                mapping: new Map([
+                    ['*.html', 'html-value'],
+                    ['*.css', 'css-value']
+                ]),
+                default: 'default-value'
+            }
+            const key = 'file.html'
+            const result = getCacheControlValue(cacheControl, key)
+
+            expect(result).toEqual('html-value')
+        })
+
+        it('should return default value', () => {
+            const cacheControl = {
+                mapping: new Map([
+                    ['*.html', 'html-value'],
+                    ['*.css', 'css-value']
+                ]),
+                default: 'default-value'
+            }
+            const key = 'file.js'
+            const result = getCacheControlValue(cacheControl, key)
+
+            expect(result).toEqual('default-value')
+        })
+
+        it('should match long path', () => {
+            const cacheControl = {
+                mapping: new Map([
+                    ['*.html', 'html-value'],
+                    ['*.css', 'css-value']
+                ]),
+                default: 'default-value'
+            }
+            const key = 'path/to/file.html'
+            const result = getCacheControlValue(cacheControl, key)
+
+            expect(result).toEqual('html-value')
+        })
+    })
+})
